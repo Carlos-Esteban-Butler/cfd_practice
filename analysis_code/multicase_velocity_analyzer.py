af62e932a5339ae45211547e6cdc934d1ba50d0d
Carlos-Esteban-Butler <carlosbutler210@gmail.com>
2025-06-29 13:38:23 +0200

    [update] .gitignore and analysis code

--- analysis_code/multicase_velocity_analyzer.py
+++ analysis_code/multicase_velocity_analyzer.py
@@ -1,5 +1,9 @@
 from foamlib import FoamCase
+import os
 
+cwd = os.getcwd()
+print(cwd)
 case = FoamCase("../elbow_cases/elbow_quad") # Load an OpenFOAM case
+print(case[0])
 for time in case:
     print(time.time)
