9f67ae59e71f1af9806ebf1df3d4c9330c24ab79
Carlos-Esteban-Butler <carlosbutler210@gmail.com>
2025-06-29 13:49:52 +0200

    [fundamental] successfully fetch the case data

--- analysis_code/multicase_velocity_analyzer.py
+++ analysis_code/multicase_velocity_analyzer.py
@@ -1,9 +1,11 @@
 from foamlib import FoamCase
 import os
 
-cwd = os.getcwd()
-print(cwd)
-case = FoamCase("../elbow_cases/elbow_quad") # Load an OpenFOAM case
+# get path to case
+parent_dir = os.path.dirname(os.path.dirname(__file__))
+pathToCase = os.path.join(parent_dir, "elbow_cases", "elbow_quad")
+# fetch foam case
+case = FoamCase(pathToCase) # Load an OpenFOAM case
 print(case[0])
 for time in case:
     print(time.time)
